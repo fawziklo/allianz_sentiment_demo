ab1a242fda8be58e6753e15365fdd263cf9500f2
R.Fawzi <fawzi.rida@hotmail.com>
2021-09-13 01:11:23 +0200

    add procfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+const fs = require('fs')
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'sentiment-demo';
+
+  try {
+  fs.unlinkSync("./node_modules/@tensorflow/tfjs-core/dist/hash_util.d.ts")
+  //file removed
+} catch(err) {
+  console.error(err)
+}
 }
